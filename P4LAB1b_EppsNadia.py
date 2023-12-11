d5ed45e434aa215914faa759376e835da90b4c90
eppsn8557 <145823339+eppsn8557@users.noreply.github.com>
2023-12-10 21:18:09 -0500

    Turtle_Initials

---
+++ P4LAB1b_EppsNadia.py
@@ -0,0 +1,42 @@
+import turtle as n
+n.shape('turtle')
+n.color('purple')
+n.pensize(5)
+#N shape
+n.right(90)
+n.forward(120)
+n.backward(120)
+n.left(40)
+n.forward(155)
+n.left(139)
+n.forward(120)
+#create e, who is a second turtle object
+#E shape
+import turtle as e
+e.shape('turtle')
+e.color('orange')
+e.penup()
+e.goto(100, 100)
+e.pensize(5)
+e.pendown()
+e.forward(110)
+e.goto(100, 100)
+e.backward(90)
+e.left(90)
+e.backward(100)
+e.right(90)
+e.left(90)
+e.forward(100)
+e.right(90)
+e.forward(90)
+e.left(90)
+e.backward(90)
+e.forward(90)
+e.right(90)
+e.forward(110)
+e.right(90)
+e.forward(90)
+
+
+
+turtle.exitonclick()
